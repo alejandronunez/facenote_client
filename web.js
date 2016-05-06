847b29d6142713f569af86afcbee6317fc6dfec4
Alejandro Nuñez Capote <alexander@inceptures.com>
2016-05-06 12:11:15 -0400

    with out dist

--- web.js
+++ web.js
@@ -3,8 +3,7 @@ var express = require('express');
 
 var app = express();
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist', "/index.html"));
+    res.sendFile(path.join(__dirname + '/app', "/index.html"));
 });
-console.log(__dirname + '/dist');
-app.use(express.static( __dirname + '/dist'));
+app.use(express.static( __dirname + '/app'));
 app.listen(process.env.PORT || 5000);
