557fa7b13b891d77f5c590a89d679fed6f48380e
Alejandro Nuñez Capote <alexander@inceptures.com>
2016-05-06 12:06:24 -0400

    update server

--- web.js
+++ web.js
@@ -5,5 +5,6 @@ var app = express();
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname + '/dist', "/index.html"));
 });
+console.log(__dirname + '/dist');
 app.use(express.static( __dirname + '/dist'));
 app.listen(process.env.PORT || 5000);
