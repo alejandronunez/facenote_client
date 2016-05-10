e7ba13fdb2b894f2627577e80de19d31028c2036
Alejandro Nuñez Capote <alexander@inceptures.com>
2016-05-10 11:03:37 -0400

    Analitic for head

--- web.js
+++ web.js
@@ -7,6 +7,7 @@ app.use('/scripts', express.static(__dirname + '/app/scripts'));
 app.use('/images', express.static(__dirname + '/app/images'));
 app.use('/styles', express.static(__dirname + '/app/styles'));
 app.use('/views', express.static(__dirname + '/app/views'));
+app.use('googlef75c21a1a476ba5f.html', express.static(__dirname + 'googlef75c21a1a476ba5f.html'));
 
 app.get("*", function (req, res) {
     //res.sendFile(path.join(__dirname + '/app', "/index.html"));
